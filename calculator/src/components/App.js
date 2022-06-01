3e0ebb888479ddb1c6b3b9feae24afa4eb40dd8c
izimio <joshua.brionne@gmail.com>
2022-06-01 17:42:22 +0200

    [PIMPING intensify :)]

--- calculator/src/components/App.js
+++ calculator/src/components/App.js
@@ -4,13 +4,23 @@ import Result from "./Result"
 import React, { useState } from "react"
 
 export default function App() {
-
+  
   const [latest, setLatest] = useState(false);
+  const [bg, setBg] = useState(() => getRandomColor());
   const [result, setResult] = useState(0);
   const [calc, setCalc] = useState({
     num: [""],
     ope: [""]
   });
+
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
   function create_expression() {
 
@@ -20,9 +30,9 @@ export default function App() {
     });
     return expression.replace("undefined", "").replace("X", "*");
   }
-  console.log(create_expression());
   function add_ope(ope) {
     ope = ope.toString();
+    setBg(getRandomColor());
 
     if (ope === "&")
       return
@@ -36,11 +46,10 @@ export default function App() {
       return
     }
     if (ope === "=") {
-      let res = eval(create_expression());
+      let res = eval(create_expression()).toString();
       res === undefined ? setResult("Error") : setResult(res);
       if (res === undefined)
         return
-      console.log(res);
       setCalc({
         num: res === 0 ? [""] : [res.toString()],
         ope: [""]
@@ -82,33 +91,39 @@ export default function App() {
     }
   }
   return (
-    <main>
-      <div className='Upper'>
-        <Result eval={create_expression()} result={result} />
-      </div>
-      <div className='Lower'>
-        <div className='Lower--numbers'>
-          <Button handleClick={add_ope} value="AC" weird={true} />
-          <Button handleClick={add_ope} value="&" weird={true} />
-          <Button handleClick={add_ope} value="%" weird={true} />
-          <Button handleClick={add_ope} value="/" operator={true} />
-          <Button handleClick={add_ope} value={7} />
-          <Button handleClick={add_ope} value={8} />
-          <Button handleClick={add_ope} value={9} />
-          <Button handleClick={add_ope} value="X" operator={true} />
-          <Button handleClick={add_ope} value={4} />
-          <Button handleClick={add_ope} value={5} />
-          <Button handleClick={add_ope} value={6} />
-          <Button handleClick={add_ope} value="-" operator={true} />
-          <Button handleClick={add_ope} value={1} />
-          <Button handleClick={add_ope} value={2} />
-          <Button handleClick={add_ope} value={3} />
-          <Button handleClick={add_ope} value="+" operator={true} />
+    <main style={
+      {
+        backgroundColor: bg,
+      }
+    }>
+      <div className='Calculator'>
+        <div className='Upper'>
+          <Result eval={create_expression()} result={result} />
         </div>
-        <div className='Lower--tricks'>
-          <Button handleClick={add_ope} value={0} extend={true} />
-          <Button handleClick={add_ope} value="." />
-          <Button handleClick={add_ope} value="=" operator={true} />
+        <div className='Lower'>
+          <div className='Lower--numbers'>
+            <Button handleClick={add_ope} value="AC" weird={true} />
+            <Button handleClick={add_ope} value="&" weird={true} />
+            <Button handleClick={add_ope} value="%" weird={true} />
+            <Button handleClick={add_ope} value="/" operator={true} />
+            <Button handleClick={add_ope} value={7} />
+            <Button handleClick={add_ope} value={8} />
+            <Button handleClick={add_ope} value={9} />
+            <Button handleClick={add_ope} value="X" operator={true} />
+            <Button handleClick={add_ope} value={4} />
+            <Button handleClick={add_ope} value={5} />
+            <Button handleClick={add_ope} value={6} />
+            <Button handleClick={add_ope} value="-" operator={true} />
+            <Button handleClick={add_ope} value={1} />
+            <Button handleClick={add_ope} value={2} />
+            <Button handleClick={add_ope} value={3} />
+            <Button handleClick={add_ope} value="+" operator={true} />
+          </div>
+          <div className='Lower--tricks'>
+            <Button handleClick={add_ope} value={0} extend={true} />
+            <Button handleClick={add_ope} value="." />
+            <Button handleClick={add_ope} value="=" operator={true} />
+          </div>
         </div>
       </div>
     </main>
--- calculator/src/index.js
+++ calculator/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
